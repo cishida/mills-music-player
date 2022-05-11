7df11c229218f5d906c5fcbde82001afca3dcfec
Christopher Ishida <christopherishida@Christophers-MacBook-Pro.local>
2022-05-11 14:15:11 -0700

    Adding test song data.

---
+++ lib/test_data/song_data.dart
@@ -0,0 +1,38 @@
+import 'package:mills_music_player/models/song/song.dart';
+
+class SongData {
+  static List<Song> songs = [
+    Song(
+      id: '18fuewhr3asdfiuhewufh',
+      name: 'Test song 1',
+      artist: 'Test artist 1',
+      duration: const Duration(
+        seconds: 201,
+      ),
+    ),
+    Song(
+      id: '7v8gahsviuehw87efhsid',
+      name: 'Test song 2',
+      artist: 'Test artist 1',
+      duration: const Duration(
+        seconds: 164,
+      ),
+    ),
+    Song(
+      id: '389fgharuh89fhw8fhwef',
+      name: 'Test song 3',
+      artist: 'Test artist 2',
+      duration: const Duration(
+        seconds: 249,
+      ),
+    ),
+    Song(
+      id: '3849tghae8whefeifh3w8',
+      name: 'Test song 4',
+      artist: 'Test artist 3',
+      duration: const Duration(
+        seconds: 193,
+      ),
+    ),
+  ];
+}
