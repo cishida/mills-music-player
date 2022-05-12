3c2256cb1df5db357d60733076a2ed6590ab8939
Christopher Ishida <christopherishida@Christophers-MacBook-Pro.local>
2022-05-12 13:29:25 -0700

    Pointing song tile view and song tile view model.

--- lib/ui/_smart_widgets/song_tile/song_tile_view.dart
+++ lib/ui/_smart_widgets/song_tile/song_tile_view.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:mills_music_player/_constants/_colors.dart';
 import 'package:mills_music_player/_utils/duration_util.dart';
 import 'package:mills_music_player/models/song/song.dart';
+import 'package:mills_music_player/ui/_smart_widgets/song_tile/song_tile_view_model.dart';
+import 'package:stacked/stacked.dart';
 
 class SongTileView extends StatelessWidget {
   const SongTileView({
@@ -13,31 +15,36 @@ class SongTileView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(right: 50),
-      color: Colors.white,
-      child: Column(
-        children: [
-          ListTile(
-            contentPadding: const EdgeInsets.only(
-              left: 16,
-            ),
-            title: Text(
-              song.title,
-            ),
-            subtitle: Text(
-              song.artist ?? '',
-            ),
-            trailing: Text(
-              DurationUtil.format(song.duration),
-            ),
+    return ViewModelBuilder.reactive(
+      viewModelBuilder: () => SongTileViewModel(),
+      builder: (context, model, child) {
+        return Container(
+          padding: const EdgeInsets.only(right: 50),
+          color: Colors.white,
+          child: Column(
+            children: [
+              ListTile(
+                contentPadding: const EdgeInsets.only(
+                  left: 16,
+                ),
+                title: Text(
+                  song.title,
+                ),
+                subtitle: Text(
+                  song.artist ?? '',
+                ),
+                trailing: Text(
+                  DurationUtil.format(song.duration),
+                ),
+              ),
+              const Divider(
+                color: ConstColors.lightDivider,
+                height: 1.0,
+              ),
+            ],
           ),
-          const Divider(
-            color: ConstColors.lightDivider,
-            height: 1.0,
-          ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
