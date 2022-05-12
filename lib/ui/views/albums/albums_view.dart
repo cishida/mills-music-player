e1bc8d5288c052ede1f140e6f9f055b2c3e95add
Christopher Ishida <christopherishida@Christophers-MacBook-Pro.local>
2022-05-12 14:24:16 -0700

    Listing albums on album view.

--- lib/ui/views/albums/albums_view.dart
+++ lib/ui/views/albums/albums_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mills_music_player/_constants/_colors.dart';
 import 'package:mills_music_player/ui/views/albums/albums_view_model.dart';
 import 'package:stacked/stacked.dart';
 
@@ -9,11 +10,43 @@ class AlbumsView extends StatelessWidget {
   Widget build(BuildContext context) {
     return ViewModelBuilder<AlbumsViewModel>.reactive(
       viewModelBuilder: () => AlbumsViewModel(),
-      builder: (context, model, child) => const Center(
-        child: Text(
-          'Albums content',
-        ),
-      ),
+      builder: (context, model, child) {
+        model.createFakerAlbums();
+
+        return ListView.builder(
+          itemCount: model.albums.length,
+          itemBuilder: (context, index) {
+            final album = model.albums[index];
+
+            return Container(
+              padding: const EdgeInsets.only(right: 50),
+              color: Colors.white,
+              child: Column(
+                children: [
+                  ListTile(
+                    contentPadding: const EdgeInsets.only(
+                      left: 16,
+                    ),
+                    title: Text(
+                      album.title,
+                    ),
+                    subtitle: Text(
+                      album.artists.join(', '),
+                    ),
+                    trailing: Text(
+                      '${album.songs.length.toString()} songs',
+                    ),
+                  ),
+                  const Divider(
+                    color: ConstColors.lightDivider,
+                    height: 1.0,
+                  ),
+                ],
+              ),
+            );
+          },
+        );
+      },
     );
   }
 }
--- lib/ui/views/albums/albums_view_model.dart
+++ lib/ui/views/albums/albums_view_model.dart
@@ -1,3 +1,17 @@
+import 'package:mills_music_player/models/album/album.dart';
+import 'package:mills_music_player/models/factories/album_factory.dart';
 import 'package:stacked/stacked.dart';
 
-class AlbumsViewModel extends BaseViewModel {}
+class AlbumsViewModel extends BaseViewModel {
+  final _albumFactory = AlbumFactory();
+
+  List<Album> _albums = [];
+  List<Album> get albums => _albums;
+
+  void createFakerAlbums() {
+    _albums = _albumFactory.generateFakeList(length: 10);
+    _albums.sort(
+      (a, b) => a.title.compareTo(b.title),
+    );
+  }
+}
